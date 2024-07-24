b0a3313c9787769d7cc1eca0fd5b56a673a34378
DennisLarsson <dennis.j.larsson@gmail.com>
2024-07-24 10:17:50 +0200

    Implement tests in CI

---
+++ tests/run_test.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+samtools faidx test_catalog.fa.gz
+
+picard CreateSequenceDictionary \
+    R=test_catalog.fa.gz \
+    O=test_catalog.fa.gz.dict
+
+bowtie2-build test_catalog.fa.gz test_catalog.fa.gz
