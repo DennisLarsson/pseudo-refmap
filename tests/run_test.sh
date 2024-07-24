4f3d27fb9fd62fdf488ae909ec7612f43d8fd693
DennisLarsson <dennis.j.larsson@gmail.com>
2024-07-24 19:58:01 +0200

    Use  picard updated syntax

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -3,7 +3,7 @@
 samtools faidx test_catalog.fa.gz
 
 picard CreateSequenceDictionary \
-    R=test_catalog.fa.gz \
-    O=test_catalog.fa.gz.dict
+    -R test_catalog.fa.gz \
+    -O test_catalog.fa.gz.dict
 
 bowtie2-build test_catalog.fa.gz test_catalog.fa.gz
