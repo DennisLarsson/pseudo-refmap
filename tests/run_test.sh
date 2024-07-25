e5157f0c38cddab17e02fdce0d884bdb298dad65
DennisLarsson <dennis.j.larsson@gmail.com>
2024-07-25 09:13:33 +0200

    Add mapping of samples against reference and tests

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -7,3 +7,17 @@ picard CreateSequenceDictionary \
     -O test_catalog.fa.gz.dict
 
 bowtie2-build test_catalog.fa.gz test_catalog.fa.gz
+
+mkdir mapped
+mkdir alignment_metrics
+
+while IFS= read -r LINE; do
+    SAMPLE_NAME=$(echo "$LINE" | cut -f1)
+
+    echo "Mapping \$SAMPLE_NAME"
+    "bowtie2 --omit-sec-seq \
+    --met-file alignment_metrics/\${SAMPLE_NAME}.log \
+    -x reference/catalog.fa.gz \
+    -U test_samples/\${SAMPLE_NAME}.fq.gz \
+    -S mapped/\${SAMPLE_NAME}.sam
+done < popmap_test
