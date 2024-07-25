2d291ec313b1a598da6075a6bf28da37d0592b47
DennisLarsson <dennis.j.larsson@gmail.com>
2024-07-25 13:05:09 +0200

    Introduce all remaining steps

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -1,17 +1,20 @@
 #! /bin/bash
 
-mkdir reference
 samtools faidx reference/catalog.fa.gz
 
 picard CreateSequenceDictionary \
     -R reference/catalog.fa.gz \
-    -O reference/catalog.fa.gz.dict
+    -O reference/catalog.dict
 
 bowtie2-build reference/catalog.fa.gz reference/catalog.fa.gz
 
 mkdir mapped
 mkdir alignment_metrics
 mkdir sorted
+mkdir mappedSortGroup
+mkdir realigned
+mkdir ref_map
+mkdir populations
 
 while IFS= read -r LINE; do
     SAMPLE_NAME=$(echo "$LINE" | cut -f1)
@@ -28,4 +31,43 @@ while IFS= read -r LINE; do
       -I mapped/${SAMPLE_NAME}.sam \
       -O sorted/${SAMPLE_NAME}.bam \
       -SO coordinate
+    
+    echo "Adding ReadGroups to $SAMPLE_NAME"
+    picard AddOrReplaceReadGroups \
+      -I sorted/${SAMPLE_NAME}.bam \
+      -O mappedSortGroup/${SAMPLE_NAME}.bam \
+      -RGID ${SAMPLE_NAME}.bam \
+      -RGLB ${SAMPLE_NAME}.bam \
+      -RGPL illumina \
+      -RGPU ${SAMPLE_NAME}.bam \
+      -RGSM ${SAMPLE_NAME}.bam
+    
+    echo "Indexing $SAMPLE_NAME"
+    samtools index mappedSortGroup/${SAMPLE_NAME}.bam
+
+    echo "Realigning $SAMPLE_NAME"
+    gatk LeftAlignIndels \
+      -R reference/catalog.fa.gz \
+      -I mappedSortGroup/${SAMPLE_NAME}.bam \
+      -O realigned/${SAMPLE_NAME}.bam
 done < popmap
+
+ref_map.pl --popmap popmap -o ref_map --samples realigned/
+
+cat ref_map/populations.sumstats.tsv | \
+  grep -v "^#" | \
+  cut -f 1,4 | \
+  sort -n | \
+  uniq | \
+  cut -f 1 | \
+  uniq -c | \
+  awk '$1 <= 10 {print $2}' > whitelist_refmap
+
+populations --in-path ref_map/ \
+  --out-path populations/ \
+  --popmap popmap \
+  -R 0.5 \
+  --min-mac 3 \
+  --vcf \
+  --write-random-snp \
+  -W whitelist_refmap
